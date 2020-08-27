2b6dd97ae681d3eb4e238b25b9b015352519b3b0
Eric M Hill <ehill111@comcast.net>
2020-08-27 08:18:00 -0400

    1. Declared CustomList member variables. 2. Created read-only property for Count. 3. Created read-only property for Capacity. 4. Instantiated CustomList.

--- MyCustomList/CustomList.cs
+++ MyCustomList/CustomList.cs
@@ -6,12 +6,43 @@ using System.Threading.Tasks;
 
 namespace CustomList
 {
-    public class CustomList<T>
-    {
-        public CustomList()
-        {
+    public class CustomList<T>//"...I want to use a custom-built list class that stores its values..."
+    {   //Member variables have/has a/an...
+        CustomList<T> _customList;
+        private int _count;
+        private int _capacity;
+        public T _dog;
 
+        //Properties
+        public int _Count//"...I want a Count property...so that I can get a count..."
+        {
+            get
+            {
+                return _count;
+            }
         }
+
+        public int _Capacity//"...I want a Capacity property...so that I can publicly see..."
+        {
+            get
+            {
+                return _capacity;
+            }
+        }
+
+        public void CreateCustomList()
+        {
+            //Methods do something.
+            _customList = new CustomList<T>();
+            _customList.Add(_dog);//Check on usage of T
+            _customList.Add(_dog);
+            _customList.Add(_dog);
+            _customList.Add(_dog);
+            _customList.Add(_dog);
+        }
+
+      
+
 
 
         public void Add(T item)
